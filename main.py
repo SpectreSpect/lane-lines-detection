5d345c04c1a5d2f189d04c5f3d5ef8b9532abaa5
KoTeuKaSeeker <emaillit8@gmail.com>
2024-07-14 07:04:17 +0400

    sdfsdf

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from src.converter.models.yolo_segmentation_model import YoloSegmentationModel
 from src.converter.containers.video_image_container import VideoImageContainer
 from src.converter.visualizer.palette.palette_register import PaletteRegister, palette_register
 import cv2
+import os
 
 def load_image(path: str):
     image = np.asarray(Image.open(path))
@@ -15,76 +16,112 @@ def load_image(path: str):
     return image
 
 if __name__ == "__main__":    
-    rtsi_core = Core(r"data\datasets\sign-detection\rtsi-filtered-100", "yolo")
-    rtsr_core = Core(r"data\datasets\sign-detection\rtsr", "yolo")
-    magistral_core = Core(r"data\datasets\sign-detection\magistral-cvat-dataset", "cvat-image")
+    # rtsi_core = Core(r"data\datasets\sign-detection\rtsi-filtered-100", "yolo")
+    # rtsr_core = Core(r"data\datasets\sign-detection\rtsr", "yolo")
+    # magistral_core = Core(r"data\datasets\sign-detection\magistral-cvat-dataset", "cvat-image")
 
-    sign_1_6_core = Core(r"data\datasets\sign-detection\signs\1_6", "cvat-image")
-    sign_1_35_core = Core(r"data\datasets\sign-detection\signs\1_35", "cvat-image")
-    sign_3_20_core = Core(r"data\datasets\sign-detection\signs\3_20", "cvat-image")
-    sign_3_22_core = Core(r"data\datasets\sign-detection\signs\3_22", "cvat-image")
-    sign_3_23_core = Core(r"data\datasets\sign-detection\signs\3_23", "cvat-image")
-    sign_6_3_2_core = Core(r"data\datasets\sign-detection\signs\6_3_2", "cvat-image")
+    # sign_1_6_core = Core(r"data\datasets\sign-detection\signs\1_6", "cvat-image")
+    # sign_1_35_core = Core(r"data\datasets\sign-detection\signs\1_35", "cvat-image")
+    # sign_3_20_core = Core(r"data\datasets\sign-detection\signs\3_20", "cvat-image")
+    # sign_3_22_core = Core(r"data\datasets\sign-detection\signs\3_22", "cvat-image")
+    # sign_3_23_core = Core(r"data\datasets\sign-detection\signs\3_23", "cvat-image")
+    # sign_6_3_2_core = Core(r"data\datasets\sign-detection\signs\6_3_2", "cvat-image")
 
-    rtsi_core.merge(rtsr_core)
-    rtsi_core.merge(magistral_core)
-    rtsi_core.merge(sign_1_6_core)
-    rtsi_core.merge(sign_1_35_core)
-    rtsi_core.merge(sign_3_20_core)
-    rtsi_core.merge(sign_3_22_core)
-    rtsi_core.merge(sign_3_23_core)
-    rtsi_core.merge(sign_6_3_2_core)
+    # rtsi_core.merge(rtsr_core)
+    # rtsi_core.merge(magistral_core)
+    # rtsi_core.merge(sign_1_6_core)
+    # rtsi_core.merge(sign_1_35_core)
+    # rtsi_core.merge(sign_3_20_core)
+    # rtsi_core.merge(sign_3_22_core)
+    # rtsi_core.merge(sign_3_23_core)
+    # rtsi_core.merge(sign_6_3_2_core)
 
-    signs = [
-        "1_1", 
-        "1_6",
-        "1_8",
-        "1_22",
-        "1_25",
-        "1_31",
-        "1_33",
-        "1_35",
-        "2_1",
-        "2_2",
-        "2_3_1",
-        "2_4",
-        "2_5",
-        "2_6",
-        "2_7",
-        "3_1",
-        "3_13",
-        "3_18_1",
-        "3_18_2",
-        "3_20",
-        "3_21",
-        "3_22",
-        "3_23",
-        "3_24",
-        "3_25",
-        "3_27",
-        "3_28",
-        "3_31",
-        "4_1_1",
-        "4_3",
-        "5_1",
-        "5_5",
-        "5_6",
-        "5_14",
-        "5_14_1",
-        "5_16",
-        "5_19_1",
-        "5_19_2",
-        "5_20",
-        "6_3_2",
-        "6_4",
-        "7_3",
-        "7_4"
-    ]
+    # signs = [
+    #     "1_1", 
+    #     "1_6",
+    #     "1_8",
+    #     "1_22",
+    #     "1_25",
+    #     "1_31",
+    #     "1_33",
+    #     "1_35",
+    #     "2_1",
+    #     "2_2",
+    #     "2_3_1",
+    #     "2_4",
+    #     "2_5",
+    #     "2_6",
+    #     "2_7",
+    #     "3_1",
+    #     "3_13",
+    #     "3_18_1",
+    #     "3_18_2",
+    #     "3_20",
+    #     "3_21",
+    #     "3_22",
+    #     "3_23",
+    #     "3_24",
+    #     "3_25",
+    #     "3_27",
+    #     "3_28",
+    #     "3_31",
+    #     "4_1_1",
+    #     "4_3",
+    #     "5_1",
+    #     "5_5",
+    #     "5_6",
+    #     "5_14",
+    #     "5_14_1",
+    #     "5_16",
+    #     "5_19_1",
+    #     "5_19_2",
+    #     "5_20",
+    #     "6_3_2",
+    #     "6_4",
+    #     "7_3",
+    #     "7_4"
+    # ]
 
-    remaining_core = rtsi_core.filter_and_split(signs, 50)
-    rtsi_core.count_annotations(verbose=1)
+    # remaining_core = rtsi_core.filter_and_split(signs, 50)
+    # rtsi_core.count_annotations(verbose=1)
 
-    remaining_core.export(r"data\datasets\sign-detection\remaining-rtsi-dataset", "yolo", 0.2)
-    rtsi_core.export(r"data\datasets\sign-detection\tld-dataset-without-annotation", "yolo", 0.2)
+    # remaining_core.export(r"data\datasets\sign-detection\remaining-rtsi-dataset", "yolo", 0.2)
+    # rtsi_core.export(r"data\datasets\sign-detection\tld-dataset-without-annotation", "yolo", 0.2)
+
+    # tld_core = Core(r"data\datasets\sign-detection\tld-dataset-without-annotation", "yolo")
+    # annotation_bundles = tld_core._annotation_bundles
+
+    # save_path = r"data\datasets\sign-detection\tld-cvat-segments"
+    # os.makedirs(save_path, exist_ok=True)
+
+    # count_parts = 7
+    # part_size = int(np.ceil(len(annotation_bundles) / count_parts))
+    # for idx in range(count_parts):
+    #     min_idx = idx * part_size
+    #     max_idx = min((idx + 1) * part_size, len(annotation_bundles))
+    #     tld_core._annotation_bundles = annotation_bundles[min_idx:max_idx]
+
+    #     segmenta_path = os.path.join(save_path, f"Segment {idx + 1}")
+    #     tld_core.export(segmenta_path, "cvat-image", 0.2)
+
+    # tsd1_core = Core(r"data\datasets\sign-detection\tld-cvat-segments\Segment 1", "cvat-image")
+    # tsd2_core = Core(r"data\datasets\sign-detection\tld-cvat-segments\Segment 2", "cvat-image")
+    # tsd3_core = Core(r"data\datasets\sign-detection\tld-cvat-segments\Segment 3", "cvat-image")
+    # tsd4_core = Core(r"data\datasets\sign-detection\tld-cvat-segments\Segment 4", "cvat-image")
+    # tsd5_core = Core(r"data\datasets\sign-detection\tld-cvat-segments\Segment 5", "cvat-image")
+    # tsd6_core = Core(r"data\datasets\sign-detection\tld-cvat-segments\Segment 6", "cvat-image")
+    # tsd7_core = Core(r"data\datasets\sign-detection\tld-cvat-segments\Segment 7", "cvat-image")
+
+    # tsd1_core.merge(tsd2_core)
+    # tsd1_core.merge(tsd3_core)
+    # tsd1_core.merge(tsd4_core)
+    # tsd1_core.merge(tsd5_core)
+    # tsd1_core.merge(tsd6_core)
+    # tsd1_core.merge(tsd7_core)
+
+    # tsd1_core.export(r"data\datasets\sign-detection\tsd-dataset-1", "yolo", validation_split=0.2)
+
+    tsd_core = Core(r"data\datasets\sign-detection\tsd-dataset-1", "yolo")
+    tsd_core.show_bundles()
 
     
--- src/converter/handlers/cvat_image_handler.py
+++ src/converter/handlers/cvat_image_handler.py
@@ -9,6 +9,8 @@ from ..data.annotation import Annotation
 from ..data.annotation_bundle import AnnotationBundle
 from ..data.box import Box
 from typing import List
+import xml.dom.minidom
+from datetime import datetime, timezone
 
 
 class CvatImageHandler(DataHandler):
@@ -84,3 +86,105 @@ class CvatImageHandler(DataHandler):
     
     def save(self, annotation_bundels: List[AnnotationBundle], label_names: List[str], path: str, validation_split: int):
         super().save(annotation_bundels, label_names, path, validation_split)
+
+        root = ET.Element("annotations")
+        
+        ET.SubElement(root, "version").text = str(1.1)
+
+        # Метаданные
+        meta = ET.SubElement(root, "meta")
+        job = ET.SubElement(meta, "job")
+
+        job_id = 1035178 + np.random.randint(0, 5000)
+        ET.SubElement(job, "id").text = str(job_id)
+        ET.SubElement(job, "size").text = str(len(annotation_bundels))
+        ET.SubElement(job, "mode").text = "annotation"
+        ET.SubElement(job, "overlap").text = str(0)
+        ET.SubElement(job, "bugtracker")
+
+        now_utc = datetime.now(timezone.utc)
+        formatted_now = now_utc.strftime("%Y-%m-%d %H:%M:%S.%f%z")
+        formatted_now = formatted_now[:-2] + ":" + formatted_now[-2:]
+
+        ET.SubElement(job, "created").text = formatted_now
+        ET.SubElement(job, "updated").text = formatted_now
+        ET.SubElement(job, "subset").text = "default"
+        ET.SubElement(job, "start_frame").text = str(0)
+        ET.SubElement(job, "stop_frame").text = str(len(annotation_bundels) - 1)
+        ET.SubElement(job, "frame_filter")
+
+        segments = ET.SubElement(job, "segments")
+        segment = ET.SubElement(segments, "segment")
+
+        ET.SubElement(segment, "id").text = str(job_id)
+        ET.SubElement(segment, "start").text = str(0)
+        ET.SubElement(segment, "stop").text = str(len(annotation_bundels) - 1)
+        ET.SubElement(segment, "url").text = f"https://app.cvat.ai/api/jobs/{job_id}"
+
+        owner = ET.SubElement(job, "owner")
+        ET.SubElement(owner, "username").text = "lit92"
+        ET.SubElement(owner, "email").text = "emaillit8@gmail.com"
+
+        ET.SubElement(job, "assignee")
+
+        labels = ET.SubElement(job, "labels")
+        for label_name in label_names:
+            label_xml = ET.SubElement(labels, "label")
+
+            ET.SubElement(label_xml, "name").text = label_name
+
+            r = np.random.randint(0, 256)
+            g = np.random.randint(0, 256)
+            b = np.random.randint(0, 256)
+            ET.SubElement(label_xml, "color").text = '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
+            ET.SubElement(label_xml, "type").text = "any"
+            ET.SubElement(label_xml, "attributes")
+        
+        ET.SubElement(meta, "dumped").text = formatted_now
+
+        for idx, bundle in enumerate(annotation_bundels):
+            image_xml = ET.SubElement(root, "image")
+            
+            image_xml.set("id", str(idx))
+            image_xml.set("name", bundle.image_container.image_name + ".jpg")
+
+            image_shape = bundle.image_container.get_image_shape()
+            image_xml.set("width", str(image_shape[0]))
+            image_xml.set("height", str(image_shape[1]))
+
+            for annotation in bundle.annotations:
+                if isinstance(annotation, Box):
+                    box = ET.SubElement(image_xml, "box")
+                    box.set("label", annotation.label)
+                    box.set("source", "manual")
+                    box.set("occluded", str(0))
+
+                    box.set("xtl", str(annotation.points[0][0]))
+                    box.set("ytl", str(annotation.points[0][1]))
+
+                    box.set("xbr", str(annotation.points[1][0]))
+                    box.set("ybr", str(annotation.points[1][1]))
+
+                    box.set("z_order", str(0))
+
+        tree = ET.ElementTree(root)
+
+        xml_str = ET.tostring(root, encoding='utf-8', method='xml')
+        dom = xml.dom.minidom.parseString(xml_str)
+        pretty_xml_as_string = dom.toprettyxml()
+
+        os.makedirs(path, exist_ok=True)
+
+        annotations_path = os.path.join(path, "annotations.xml")
+
+        with open(annotations_path, "w", encoding='utf-8') as file:
+            file.write(pretty_xml_as_string)
+        
+        images_path = os.path.join(path, "images")
+
+        os.makedirs(images_path, exist_ok=True)
+
+        for bundle in annotation_bundels:
+            bundle.image_container.save_image(images_path)
+        
